813996a0385fd6ad0bc172f83e85e287ea253cc2
vendi174 <poojinvr@gmail.com>
2017-07-29 22:20:33 +0530

    first version of Prime

---
+++ Prime.java
@@ -0,0 +1,58 @@
+package programs;
+import java.util.*;
+public class Prime {
+	public static void main(String args[])
+	{
+	 int n =70;
+	 int c =0;
+	 ArrayList<Integer> primeList = new ArrayList<Integer>();
+	 ArrayList<Integer> list = new ArrayList<Integer>();
+	 primeList.add(2);
+	 list.add(2);
+	 for(int i=3;i<=n;i++)
+	 {
+		 if(isPrime(i))
+		 {
+		 	primeList.add(i);
+		 	list.add(i);
+	 
+		 }
+	 }
+	 
+	 
+	for(int i=0;i<primeList.size();i++)
+	{
+		for(int j = 0;j<primeList.size();j++)
+		{
+			String str = primeList.get(i).toString()+primeList.get(j);
+			int in = Integer.parseInt(str);
+			
+			if(list.contains(in))
+				c++;
+			else
+			{
+				if(isPrime(in))
+				{
+					
+					c++;
+				}
+			}
+		}
+	}
+	
+	System.out.println(c);
+	 
+	
+	
+	}
+
+	private static Boolean isPrime(int i) {
+		for(int j = 2;j<=Math.sqrt(i);j++)
+		{
+			if(i%j==0)
+				return false;
+		}
+		return true;
+		
+	}
+}
